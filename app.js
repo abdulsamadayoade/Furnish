2ac2cbd19e29c60aae131fc6c3efb5a324e7a790
AbdulSamad Ayoade <66149753+abdulsamadayoade@users.noreply.github.com>
2020-09-02 17:39:24 +0100

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var MenuItems = document.getElementById("MenuItems");
 
-// MenuItems.style.maxHeight = "0px";
+MenuItems.style.maxHeight = "0px";
 
 function menutoggle() {
     if (MenuItems.style.maxHeight == "0px") {
