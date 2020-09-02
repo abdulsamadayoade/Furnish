b9b57efec589ce207c20edd636caecf8553e0f43
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2020-09-02 17:11:51 +0100

    Create app.js
    App.js is the main javascript for this project. Currently, the file contains few lines of codes for the mobile navigation menu

---
+++ app.js
@@ -0,0 +1,11 @@
+var MenuItems = document.getElementById("MenuItems");
+
+// MenuItems.style.maxHeight = "0px";
+
+function menutoggle() {
+    if (MenuItems.style.maxHeight == "0px") {
+        MenuItems.style.maxHeight = "200px";
+    } else {
+        MenuItems.style.maxHeight = "0px";
+    }
+}
