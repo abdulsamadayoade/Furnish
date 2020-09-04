50c85b11578c989de2dd4df86dba4bc80e4d9e87
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2020-09-04 23:39:50 +0100

    create the account page

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ function menutoggle() {
         MenuItems.style.maxHeight = "0px";
     }
 }
+
+
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+
+
+function register() {
+    RegForm.style.transform = "translateX(0px)";
+    LoginForm.style.transform = "translateX(0px)";
+    Indicator.style.transform = "translateX(100px)";
+}
+
+function login() {
+    RegForm.style.transform = "translateX(300px)";
+    LoginForm.style.transform = "translateX(300px)";
+    Indicator.style.transform = "translateX(0px)";
+}
