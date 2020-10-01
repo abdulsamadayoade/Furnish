304333dd87e449c97345e5d460b4b9ecc12600c6
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2020-10-01 07:27:23 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-var MenuItems = document.getElementById("MenuItems");
-
-MenuItems.style.maxHeight = "0px";
-
-function menutoggle() {
-    if (MenuItems.style.maxHeight == "0px") {
-        MenuItems.style.maxHeight = "200px";
-    } else {
-        MenuItems.style.maxHeight = "0px";
-    }
-}
-
-
 var LoginForm = document.getElementById("LoginForm");
 var RegForm = document.getElementById("RegForm");
 var Indicator = document.getElementById("Indicator");
